e5272dadf9a795a7d2ccc373b6b9d94a9e214a49
Tibnaamer <tibnaamer@gmail.com>
2026-07-22 21:04:44 +0100

    Small backend adjustments made
    Small backend adjustments made

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=600)
+    author = models.CharField(max_length=50)
+    date_created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return self.name
+        return self.title
