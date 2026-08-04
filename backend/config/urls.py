f7b0403d350e1737c8bd29ddce588d4936f367d7
Tibnaamer <tibnaamer@gmail.com>
2026-08-04 16:50:40 +0100

    Updated Settings.py and urls.py
    * Updated Settings.py and urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('api/', include('courses.urls')),
     path('api/', include(('students.urls', 'students'), namespace='students-api')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
